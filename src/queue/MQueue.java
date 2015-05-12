644e133474711c0e768e7b49f9150619191e0f9b
yeqianfeng <yeqianfeng911@gmail.com>
2015-05-12 20:58:10 +0800

    initial commit

---
+++ src/queue/MQueue.java
@@ -0,0 +1,96 @@
+package queue;
+
+import java.sql.SQLSyntaxErrorException;
+import java.util.Iterator;
+
+/**
+ * Created by yeqf on 5/12/15.
+ */
+public class MQueue<T> implements Iterable<T> {
+    private Node head;
+    private Node tail;
+    private int N;
+
+    public MQueue() {
+        head = tail = null;
+        N = 0;
+    }
+
+    public static void main(String[] args) {
+        MQueue<String> q = new MQueue<String>();
+        String a = "Hello ";
+        String b = "I am ";
+        String c = "yeqianfeng";
+        q.enQueue(a);
+        q.enQueue(b);
+        System.out.println("-------"+q.size());
+        System.out.println(q.deQueue());
+        System.out.println("-------"+q.size());
+        q.enQueue(c);
+        System.out.println("-------"+q.size());
+        Iterator<String> iter = q.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void enQueue(T t) {
+        Node oldtail = tail;
+        tail = new Node();
+        tail.item = t;
+        tail.next = null;
+        if (isEmpty())
+            head = tail;
+        else oldtail.next = tail;
+        N++;
+    }
+
+    public T deQueue() {
+        T t = head.item;
+        head = head.next;
+        N--;
+        if (isEmpty())
+            tail = head;
+        return t;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new IterListen();
+    }
+
+    private class Node {
+        T item;
+        Node next;
+    }
+
+    private class IterListen implements Iterator<T> {
+        private Node temp;
+        //private int i;
+
+        public IterListen() {
+            temp = head;
+            //i = N;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return temp != null;
+        }
+
+        @Override
+        public T next() {
+            T t = temp.item;
+            temp = temp.next;
+            return t;
+        }
+    }
+}
---
+++ src/stack/MStack.java
@@ -0,0 +1,87 @@
+package stack;
+
+import java.util.Iterator;
+
+/**
+ * Created by yeqianfeng on 5/12/15.
+ */
+public class MStack<T> implements Iterable<T> {
+
+    private int N;
+    private Node first;
+
+    public MStack() {
+        first = null;
+        N = 0;
+
+    }
+
+    public static void main(String[] args) {
+        MStack<String> s = new MStack<String>();
+        String a = "yeqianfeng";
+        String b = "I am ";
+        String c = "Hello ";
+        s.push(a);
+        s.push(b);
+        s.push(c);
+        System.out.print(s.pop() + s.pop() + s.pop());
+        System.out.print("a");
+    }
+
+    public boolean ifEmpty() {
+        return first == null;
+
+    }
+
+    public int size() {
+        return N;
+
+    }
+
+    public void push(T t) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = t;
+        N++;
+        first.next = oldfirst;
+    }
+
+    public T pop() {
+        T t = first.item;
+        first = first.next;
+        N--;
+        return t;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new IterLisner();
+    }
+
+    private class Node {
+        private T item;
+        private Node next;
+    }
+
+    private class IterLisner implements Iterator<T> {
+        private Node temp;
+        private int i;
+
+        public IterLisner() {
+            temp = first;
+            i = N;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return temp != null;
+        }
+
+        @Override
+        public T next() {
+            T t = temp.item;
+            temp = temp.next;
+            return t;
+        }
+    }
+}
