745e44c6560962c59b0b0584dc8bc5010579df6f
yeqianfeng <yeqianfeng911@gmail.com>
2015-05-13 11:18:38 +0800

    Add class Mbag

--- src/bag/MBag.java
+++ src/bag/MBag.java
@@ -1,63 +1,80 @@
 package bag;
 
 import java.util.Iterator;
+import java.util.Map;
 
-/*************************************************************************
-    > File Name: MBag.java
-    > Author: yeqianfeng
-    > Mail: yeqianfeng911@gmail.com 
-    > Created Time: Wed 13 May 2015 10:47:48 AM CST
- ************************************************************************/
+/**
+ * **********************************************************************
+ * > File Name: MBag.java
+ * > Author: yeqianfeng
+ * > Mail: yeqianfeng911@gmail.com
+ * > Created Time: Wed 13 May 2015 10:47:48 AM CST
+ * **********************************************************************
+ */
 public class MBag<T> implements Iterable<T> {
-	private Node first;
-	private int N;
+    private Node first;
+    private int N;
 
-	public MBag(){
-		first = null;
-		N = 0;
-	}
+    public MBag() {
+        first = null;
+        N = 0;
+    }
 
-	public boolean isEmpty(){
-		return first == null;
-	}
-	
-	public int size(){
-		return N;
-	}
+    public boolean isEmpty() {
+        return first == null;
+    }
 
-	public void addItem(T t) {
-		Node oldfirst = first;
-		first = new Node();
-		first.item = t;
-		oldfirst.next = first;
-		N++;
-	}
+    public int size() {
+        return N;
+    }
 
-	@Override
-	public Iterator<T> iterator(){
-		return new IterListen();
-	}
+    public void addItem(T t) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = t;
+        first.next = oldfirst;
+        N++;
+    }
 
-	private class IterListen implements Iterator<T> {
-		private Node temp;
+    @Override
+    public Iterator<T> iterator() {
+        return new IterListen();
+    }
 
-		public IterListen(){
-			temp = first;
-		}
-		
-		public boolean hasNext(){
-			return temp != null;
-		}
+    private class IterListen implements Iterator<T> {
+        private Node temp;
 
-		public T next() {
-			T t = temp.item;
-			temp = temp.next;
-			return t;
-		}
-	}
+        public IterListen() {
+            temp = first;
+        }
 
-	private class Node {
-		T item;
-		Node next;
-	}
+        public boolean hasNext() {
+            return temp != null;
+        }
+
+        public T next() {
+            T t = temp.item;
+            temp = temp.next;
+            return t;
+        }
+    }
+
+    private class Node {
+        T item;
+        Node next;
+    }
+    
+    public static void main(String[] args) {
+        MBag<String> b = new MBag<>();
+        String a = "Hello ";
+        String s = "I am ";
+        String c = "yeqianfeng";
+        b.addItem(a);
+        b.addItem(s);
+        b.addItem(c);
+        Iterator<String> iter = b.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        }
+    }
 }
