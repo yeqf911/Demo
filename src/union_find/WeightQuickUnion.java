a1308de965166ba6e4257715f588b5cd5ca78d0a
yeqianfeng <yeqianfeng911@gmail.com>
2015-05-14 22:08:16 +0800

    Add WeightQuickUnion and the file downloaded online

---
+++ src/union_find/WeightQuickUnion.java
@@ -0,0 +1,94 @@
+package union_find;
+
+import java.util.Scanner;
+
+/**
+ * Created by yeqf on 5/13/15.
+ */
+public class WeightQuickUnion {
+    private int[] id;
+    private int[] sr;
+    private int count;
+
+    public WeightQuickUnion(int N) {
+        id = new int[N];
+        sr = new int[N];
+        count = N;
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sr[i] = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        UnionFind uf = new UnionFind(N);
+		StopWatch timer = new StopWatch();
+        while (in.hasNext()){
+            int p = in.nextInt();
+            int q = in.nextInt();
+            if (uf.connect(p, q))
+                continue;
+            uf.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        System.out.println("count: " + uf.getCount());
+		double time = timer.elapsedTime();
+		System.out.println("time :" + time);
+       // uf.displayId();
+        System.out.println();
+
+    }
+
+    public void union(int p, int q) {
+        int rootp = find(p);
+        int rootq = find(q);
+        if (rootp == rootq)
+            return;
+        if (sr[p] < sr[q]) {
+            id[rootp] = rootq;
+            sr[q] += sr[p];
+        }
+        else {
+            id[rootq] = rootp;
+            sr[p] += sr[q];
+        }
+        count--;
+    }
+/**
+* optimized the QuickUnion
+ **/
+    public int find(int p) {
+        while (p != id[p])
+            p = id[p];
+        return p;
+    }
+
+    public boolean connect(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void displayId() {
+        for (int i = 0; i < id.length; i++) {
+            System.out.print(find(i)+" ");
+        }
+    }
+}
+
+class StopWatch{
+    private final long start;
+
+    public StopWatch(){
+        start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime(){
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+}
---
+++ src/union_find/WeightedQuickUnionUF.java
@@ -0,0 +1,118 @@
+package union_find;
+
+import java.util.Scanner;
+
+/**
+ * Created by yeqf on 5/14/15.
+ */
+public class WeightedQuickUnionUF {
+    private int[] parent;   // parent[i] = parent of i
+    private int[] size;     // size[i] = number of objects in subtree rooted at i
+    private int count;      // number of components
+
+    /**
+     * Initializes an empty union-find data structure with N isolated components 0 through N-1.
+     *
+     * @param N the number of objects
+     * @throws java.lang.IllegalArgumentException if N < 0
+     */
+    public WeightedQuickUnionUF(int N) {
+        count = N;
+        parent = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and N-1) from standard input,
+     * where each integer represents some object;
+     * if the objects are in different components, merge the two components
+     * and print the pair to standard output.
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N);
+        while (in.hasNext()) {
+            int p = in.nextInt();
+            int q = in.nextInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            System.out.println(p + " " + q);
+        }
+        System.out.println(uf.count() + " components");
+    }
+
+    /**
+     * Returns the number of components.
+     *
+     * @return the number of components (between 1 and N)
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns the component identifier for the component containing site <tt>p</tt>.
+     *
+     * @param p the integer representing one site
+     * @return the component identifier for the component containing site <tt>p</tt>
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= p < N
+     */
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int N = parent.length;
+        if (p < 0 || p >= N) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + N);
+        }
+    }
+
+    /**
+     * Are the two sites <tt>p</tt> and <tt>q</tt> in the same component?
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @return <tt>true</tt> if the two sites <tt>p</tt> and <tt>q</tt>
+     * are in the same component, and <tt>false</tt> otherwise
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the component containing site<tt>p</tt> with the component
+     * containing site <tt>q</tt>.
+     *
+     * @param p the integer representing one site
+     * @param q the integer representing the other site
+     * @throws java.lang.IndexOutOfBoundsException unless both 0 <= p < N and 0 <= q < N
+     */
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        count--;
+    }
+
+}
+
