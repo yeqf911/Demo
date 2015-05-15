c63aa50e9ae7f019f5b0dfc07b5242bae833757b
yeqianfeng <yeqianfeng911@gmail.com>
2015-05-15 18:28:20 +0800

    Add the Quick-union with path compression

--- src/union_find/RedQuickUnion.java
+++ src/union_find/RedQuickUnion.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 /**
  * Created by yeqf on 5/13/15.
  */
-public class UnionFind {
+public class RedQuickUnion {
     private int[] id;
     private int count;
-	private int num;
+    private int num;
 
-    public UnionFind(int N) {
+    public RedQuickUnion(int N) {
         id = new int[N];
         count = N;
         for (int i = 0; i < N; i++) {
@@ -19,61 +19,76 @@ public class UnionFind {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        UnionFind uf = new UnionFind(N);
-        while (in.hasNext()){
+        RedQuickUnion uf = new RedQuickUnion(N);
+        while (in.hasNext()) {
             int p = in.nextInt();
             int q = in.nextInt();
+			//if(p == 655302)
+			//	System.exit(0);
             if (uf.connect(p, q))
                 continue;
             uf.union(p, q);
             System.out.println(p + " " + q);
-			uf.displayId();
-			System.out.println(uf.getNum());
+           // uf.displayId();
+            //System.out.println(uf.getNum());
         }
         System.out.println("count: " + uf.getCount());
         System.out.println();
-
     }
 
     public void union(int p, int q) {
-        int pId = find(p);
-		num++;
-        int qId = find(q);
-		num++;
-        if (pId == qId)
+        int rootp = find(p);
+		int rootq = find(q);
+        if (rootp == rootq)
             return;
-        for (int i = 0; i < id.length; i++) {
-			num++;
-            if (id[i] == pId){
-				num++;
-                id[i] = qId;
-			}
-        }
+		id[rootq] = rootp;
+       // num++;
         count--;
     }
 
     public int find(int p) {
-		num++;
-        return id[p];
+		int root = p;
+        while (root != id[root]) {
+            root = id[root];
+       //     num += 2;
+        }
+		while (p != root){
+			int n = id[p];
+			id[p] = root;
+			p = n;
+		}
+        return root;
     }
 
     public boolean connect(int p, int q) {
-		num += 2;
         return find(p) == find(q);
     }
 
     public int getCount() {
         return count;
     }
-	
-	public int getNum(){
-		return num;
-	}
+
+    public int getNum() {
+        return num;
+    }
 
     public void displayId() {
         for (int i = 0; i < id.length; i++) {
-            System.out.print(find(i)+" ");
+            System.out.print(find(i) + " ");
         }
-		System.out.println();
+        System.out.println();
+    }
+}
+
+class StopWatch {
+    private final long start;
+
+    public StopWatch() {
+        start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
     }
 }
