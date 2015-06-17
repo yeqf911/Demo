562600e4c7cd19142e72cc9a83794af41c812381
yeqianfeng <yeqianfeng911@gmail.com>
2015-06-17 12:51:37 +0800

    add Set

--- src/collection/set/SetDemo01.java
+++ src/collection/set/SetDemo01.java
@@ -6,17 +6,25 @@ import java.util.TreeSet;
 
 /**
  * Created by yeqf on 2015/6/17.
+ *
  */
 public class SetDemo01 {
 
+    /**
+     * 可见Set集合中的元素是不能有重复的
+     * @param args
+     */
     public static void main(String[] args) {
         Set<String> set;
+        /**
+         * HashSet是不能保证元素是有序的，他只是按照元素的哈希值来给元素安排存储单元
+         */
         set = new HashSet<>();
         set.add("A");
-        set.add("B");
+        set.add("BF");
         set.add("C");
         set.add("F");
-        set.add("E");
+        set.add("EG");
         set.add("D");
         set.add("A");
 
@@ -24,13 +32,17 @@ public class SetDemo01 {
         System.out.println(set);
 
 
+        /**
+         * 而TreeSet是有序的
+         */
         Set<String> set1 = new TreeSet<>();
-        set1.add("A");
-        set1.add("B");
+        set1.add("ADB");
+        set1.add("BA");
         set1.add("C");
         set1.add("F");
         set1.add("E");
         set1.add("D");
+        set1.add("AB");
         System.out.println(set1);
     }
 }
